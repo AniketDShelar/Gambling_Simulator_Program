c4084108d5493222a1a2928e509f135531e97b5c
Aniket Shelar <theaniketshelar@gmail.com>
2022-08-18 13:46:39 +0530

    Added: Welcome message

---
+++ src/com/bridgelabz/gamblingsimulatorprogram/GamblingSimulator.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.gamblingsimulatorprogram;
+
+public class GamblingSimulator {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Gambling Simulator Program");
+    }
+}
