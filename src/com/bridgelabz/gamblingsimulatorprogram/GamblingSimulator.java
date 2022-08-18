db328162aaf8529cfedde07a4738c003fdaefab1
Aniket Shelar <theaniketshelar@gmail.com>
2022-08-18 13:55:12 +0530

    Added: either win or lose

--- src/com/bridgelabz/gamblingsimulatorprogram/GamblingSimulator.java
+++ src/com/bridgelabz/gamblingsimulatorprogram/GamblingSimulator.java
@@ -2,8 +2,18 @@ package com.bridgelabz.gamblingsimulatorprogram;
 
 public class GamblingSimulator {
     int stake = 100;
-    final int dailybet = 1;
+    final static int dailyBet = 1;
+
+    public static void gambler() {
+        int result = (int) Math.floor(Math.random() * 10 % 2);
+        if(dailyBet == result) {
+            System.out.println("Winner");
+        }else {
+            System.out.println("loser");
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulator Program");
+        gambler();
     }
 }
