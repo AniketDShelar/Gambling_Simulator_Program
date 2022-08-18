1276a5cbf3d1a372fac7ff5db3c09bc731e4e3df
Aniket Shelar <theaniketshelar@gmail.com>
2022-08-18 15:21:50 +0530

    Added: how many days won or lose and how much

--- src/com/bridgelabz/gamblingsimulatorprogram/GamblingSimulator.java
+++ src/com/bridgelabz/gamblingsimulatorprogram/GamblingSimulator.java
@@ -3,19 +3,35 @@ package com.bridgelabz.gamblingsimulatorprogram;
 public class GamblingSimulator {
     static int stake = 100;
     final static int dailyBet = 1;
+    static final int numOfDays = 20;
+    static int wins = 0;
+    static int lose = 0;
+    static int stakeWin = 0;
+    static int stakeLose = 0;
     public static void gambler() {
+        for (int i = 1; i <= numOfDays; i++) {
             int result = (int) Math.floor(Math.random() * 10 % 2);
             if (dailyBet == result) {
                 System.out.println("Winner");
-                stake = stake + 50;
-                System.out.println("stake after winning today : $" +stake);
+                stakeWin = stake + 50;
+                stake = stakeWin;
+                System.out.println("Day " +i+ " Stake after Win : $" +stakeWin);
+                wins++;
+                stakeWin++;
             } else {
                 System.out.println("loser");
-                stake = stake - 50;
-                System.out.println("stake after losing today : $" +stake);
+                stakeLose = stake - 50;
+                stake = stakeLose;
+                System.out.println("Day " +i+ " Stake after Losings : $" +stake);
+                lose++;
+                stakeLose++;
             }
         }
 
+        System.out.println("Stake left after 20 days: $" + (stakeWin + stakeLose));
+        System.out.println("Total wins in month : " +wins+ " Stake won in month : $" +stakeWin);
+        System.out.println("Total loses in month : " +lose+ " Stake lose in month $: " +stakeLose);
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulator Program");
         gambler();
